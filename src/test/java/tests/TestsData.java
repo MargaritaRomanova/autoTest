dfcae1d97b2aaefc4dbd3f8e59c8be60aac43956
MargaritaRomanova <steklova.margarita@yandex.ru>
2021-10-14 20:40:27 +0300

    test commit

--- src/test/java/tests/TestsData.java
+++ src/test/java/tests/TestsData.java
@@ -5,6 +5,6 @@ import org.testng.annotations.DataProvider;
 public class TestsData {
     @DataProvider(name = "searchDataProvider")
     public Object[][] dataProviderMethod() {
-        return new Object[][]{{"selenium java"}};
+        return new Object[][]{{"selenium java"}, {"java"}};
     }
 }
